f627796c04ce13eacc1d3672474edfa152c2fe0a
Alex <git@mohr.io>
2017-07-26 01:20:59 +0200

    Fixed print instead of return

--- src/icongetter.py
+++ src/icongetter.py
@@ -6,13 +6,13 @@ def __init__():
 
 def get_icon_svg_path(name):
     name_parts = name.split(".")
-    file_path = os.path.join(os.path.dirname(sys.argv[0]), '..', 'lib', 'material-design-icons',
+    file_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', 'lib', 'material-design-icons',
                             name_parts[0], "svg", "production",
                              "ic_" + name_parts[1] + "_24px.svg")
 
     file = open(file_path, 'r')
     data = file.read()
     xml = ET.fromstring(data)
-    print(xml[0].attrib["d"])
+    return xml[0].attrib["d"]
 
 
